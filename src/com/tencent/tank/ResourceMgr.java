df1f0348220df8e57cc3818f6ac799a737e99410
jason1983-git2 <76798054+jason1983-git@users.noreply.github.com>
2022-05-24 17:57:59 +0800

    建立ResourceMgr类管理资源，把图片load到内存，坦克换成图片版

---
+++ src/com/tencent/tank/ResourceMgr.java
@@ -0,0 +1,27 @@
+package com.tencent.tank;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * @Auther: Yu Panpan
+ * @Date: 2022/5/24 - 05 - 24 - 17:55
+ * @Description: com.tencent.tank
+ * @version: 1.0
+ */
+public class ResourceMgr {
+
+    public static BufferedImage tankL,tankU,tankR,tankD;
+
+    static{
+        try {
+            tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
+            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
+            tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
+            tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/com/tencent/tank/Tank.java
+++ src/com/tencent/tank/Tank.java
@@ -37,10 +37,20 @@ public class Tank {
     }
 
     public void paint(Graphics g){
-        Color c = g.getColor();
-        g.setColor(Color.BLUE);
-        g.fillRect(x,y,50,50);
-        g.setColor(c);
+        switch (dir){
+            case LEFT:
+                g.drawImage(ResourceMgr.tankL,x,y,null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.tankU,x,y,null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.tankR,x,y,null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.tankD,x,y,null);
+                break;
+        }
 
         move();
     }
