960cbe7f2cf119c08937127866e788366b4550b2
jason1983-git2 <76798054+jason1983-git@users.noreply.github.com>
2022-06-09 17:10:50 +0800

    调整爆炸的位置

--- src/com/tencent/tank/Bullet.java
+++ src/com/tencent/tank/Bullet.java
@@ -89,7 +89,9 @@ public class Bullet {
         if(bRect.intersects(tRect)){
             tank.die();
             this.die();
-            tf.explodes.add(new Explode(this.x,this.y,tf));
+            int eX = tank.getX() + Tank.WIDTH/2 -Explode.WIDTH/2;
+            int eY = tank.getY() + Tank.HEIGHT/2 - Explode.HEIGHT/2;
+            tf.explodes.add(new Explode(eX,eY,tf));
         }
     }
 
